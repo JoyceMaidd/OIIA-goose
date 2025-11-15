49a14d7fc7a82c3c6e262ddca4ee295be6123b1a
JoyceMaidd <joycemickeyfish@gmail.com>
2025-11-15 08:11:23 -0500

    Add unused inputs

--- src/project.sv
+++ src/project.sv
@@ -17,9 +17,10 @@ module tt_um_goose (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uio_out[6:0] = 7'b0000000;
-  assign uio_oe = 8'b10000000;
-
+  assign uio_out = 0;
+  assign uio_oe  = 0;
   graphics gfx(.clk(clk), .rst_n(rst_n), .vga_pmod(uo_out));
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, ui_in, uio_in, 1'b0};
 
 endmodule
