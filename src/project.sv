ebb366e5c46a4c2575a1e1605cee2f357f57374a
JoyceMaidd <joycemickeyfish@gmail.com>
2025-11-15 07:57:42 -0500

    Test on graphics.sv

--- src/project.sv
+++ src/project.sv
@@ -17,11 +17,10 @@ module tt_um_goose (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+  graphics gfx(.clk(clk), .rst_n(rst_n), .vga_pmod(uo_out));
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, ui_in, uio_in, 1'b0};
 
 endmodule
