dcc18237e90b0bf8ea019732dc80fc279168cf85
Victoria Mak <vmak@uwaterloo.ca>
2025-11-23 15:11:24 -0500

    Included frames in the top project for the animation

--- src/project.sv
+++ src/project.sv
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_goose(
@@ -15,11 +10,6 @@ module tt_um_goose(
     input  wire       clk,
     input  wire       rst_n
 );
-    assign uio_out = 0;
-    assign uio_oe  = 0;
-    
-    // List all unused inputs to prevent warnings
-    wire _unused = &{ena, clk, ui_in, uio_in, 1'b0};
 
     //------------------------------------------------------------
     // VGA signals
@@ -56,14 +46,33 @@ module tt_um_goose(
     // You can store 2-bit or 4-bit indices per pixel.
     // Here: 6-bit address, 4-bit output = 16-color palette
     //------------------------------------------------------------
-    wire [2:0] pixel_index;
+    wire [2:0] pixel_index0;
+    wire [2:0] pixel_index1;
+    wire [2:0] pixel_index2;
+    wire [2:0] pixel_index3;
 
     wire in_shape = pix_x[8] && !pix_x[9] && !pix_y[8];
 
-    frame0_lut frame0_inst (
-        .x(pix_x[7:3]),   // downsample to 64 pixels horizontally
-        .y(pix_y[7:3]),   // downsample to 64 pixels vertically
-        .pixel(pixel_index)
+    reg [2:0] pixel_index;
+
+    always @* begin
+        case (frame_num)
+            2'b00: pixel_index = pixel_index0;
+            2'b01: pixel_index = pixel_index1;
+            2'b10: pixel_index = pixel_index2;
+            2'b11: pixel_index = pixel_index3;
+            default: pixel_index = 0;
+        endcase
+    end
+
+
+    frame0_lut frame0 (
+        .x(pix_x[7:3]),
+        .y(pix_y[7:3]),
+        .pixel0(pixel_index0),
+        .pixel1(pixel_index1),
+        .pixel2(pixel_index2),
+        .pixel3(pixel_index3)
     );
 
     //------------------------------------------------------------
@@ -73,7 +82,7 @@ module tt_um_goose(
     //------------------------------------------------------------
     wire [1:0] pal_r, pal_g, pal_b;
 
-    palette_lut palette_inst (
+    palette_lut palette (
         .index(pixel_index),
         .subpixel(pix_x[1:0] ^ pix_y[1:0]), // Dithering (blending colours using subpixels)
         .r(pal_r),
@@ -84,8 +93,30 @@ module tt_um_goose(
     //------------------------------------------------------------
     // Drive video
     //------------------------------------------------------------
+    // assign R = video_active ? in_shape ? pal_r : (pix_x == frame_num * 100) ? 2'b11 : 2'b00 : 2'b00;
+    // assign G = video_active ? in_shape ? pal_g : (pix_x == frame_num * 100) ? 2'b11 : 2'b00 : 2'b00;
+    // assign B = video_active ? in_shape ? pal_b : (pix_x == frame_num * 100) ? 2'b11 : 2'b00 : 2'b00;
+
     assign R = video_active ? in_shape ? pal_r : 2'b00 : 2'b00;
     assign G = video_active ? in_shape ? pal_g : 2'b00 : 2'b00;
     assign B = video_active ? in_shape ? pal_b : 2'b00 : 2'b00;
+
+    reg [5:0] frame_counter;
+    reg [1:0] frame_num;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            frame_counter <= 0;
+            frame_num <= 0;
+        end else begin
+            if (frame_counter == 10) begin
+                frame_counter <= 0;
+                frame_num <= frame_num + 1;
+            end else if (pix_x == 0 && pix_y == 0) begin
+                frame_counter <= frame_counter + 1;
+            end
+        end
+    end
+
 
 endmodule
